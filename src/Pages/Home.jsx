bb56fbb8f9f6a4c55cf73e04c4eb91bae0f05b13
Arafat1652 <arafathosen4042@gmail.com>
2024-06-10 19:35:27 +0600

    favicon change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet-async";
 import Story from "../components/AllStory/Story";
 import Banner from "../components/Banner/Banner";
 import TourAndTravelGuide from "../components/HomeComponents/TourAndTravelGuide";
@@ -7,7 +8,9 @@ import World from "../components/World/World";
 
 const Home = () => {
     return (
-        <div>
+        <div><Helmet>
+            <title>Home || Travela</title>
+        </Helmet>
             <Banner/>
             <TourAndTravelGuide/>
             <TourType/>
